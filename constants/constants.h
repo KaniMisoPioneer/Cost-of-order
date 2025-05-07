ada2bde38bfba439a85ebd03ff47ade9025cf2c9
KaniMisoPioneer <aleksey5553535@gmail.com>
2025-05-07 10:53:12 +0300

    Add constants

---
+++ constants/constants.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+// Базовые пути
+#define DIR_TEMPLATES   "./templates/"
+#define DIR_CSS         "./css/"
+// Пути к HTML-шаблонам
+#define ORDER_FORM_HTML     DIR_TEMPLATES "order_form.html"
+#define ORDER_RESULT_HTML   DIR_TEMPLATES "order_result.html"
+// Путь к CSS
+#define PATH_CSS_STYLES     DIR_CSS "styles.css"
+// Content-Type заголовки
+#define CONTENT_TYPE_HTML   "Content-Type: text/html; charset=utf-8\r\n"
+#define CONTENT_TYPE_CSS    "Content-Type: text/css; charset=utf-8\r\n"
+#endif
